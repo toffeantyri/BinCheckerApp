c7931a4a1eca8a72ac4dc9cbaf4dbd0aae50f1e2
toffeantyri <toffeantyri@gmail.com>
2022-12-21 15:20:25 +0500

    app: add application class Manifest: add custom app, add permission network

---
+++ app/src/main/java/ru/testwork/bincheckerapp/app/BinCheckApplication.kt
@@ -0,0 +1,9 @@
+package ru.testwork.bincheckerapp.app
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BinCheckApplication : Application() {
+
+}
